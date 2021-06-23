417b6e4da5fb045feb4b1e644dd1337362708fb4
d1eshi <silva.diegoF03@gmail.com>
2021-06-23 11:15:39 -0300

    add lazy loading and fixed bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
 import './header'
 import './projects'
 import './modalGmail'
+
+if ('loading' in HTMLImageElement.prototype) {
+	console.log('El navegador soporta `lazy-loading`...')
+} else {
+	console.log('`lazy-loading` no soportado...')
+}
